e11c159da0dfeed419531dc4789e5e86b57fd71b
shino <827153658@qq.com>
2019-03-18 19:04:30 +0800

    leetcode141go

--- golang/141.环形链表.go
+++ golang/141.环形链表.go
@@ -59,6 +59,17 @@
  * }
  */
 func hasCycle(head *ListNode) bool {
-    
+    if head != nil {
+		slow := head
+		fast := head
+		for fast != nil && fast.Next != nil {
+			slow = slow.Next
+			fast = fast.Next.Next
+			if slow == fast {
+				return true
+			}
+		}
+	}
+	return false
 }
 
