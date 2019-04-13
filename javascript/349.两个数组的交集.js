459fd6cf6f67228ff7d6ad29be38499b0ac87dc1
shino <shinoyasha@163.com>
2019-04-13 17:38:55 +0800

    leetcode349

---
+++ javascript/349.两个数组的交集.js
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=349 lang=javascript
+ *
+ * [349] 两个数组的交集
+ *
+ * https://leetcode-cn.com/problems/intersection-of-two-arrays/description/
+ *
+ * algorithms
+ * Easy (61.50%)
+ * Total Accepted:    17.6K
+ * Total Submissions: 28.6K
+ * Testcase Example:  '[1,2,2,1]\n[2,2]'
+ *
+ * 给定两个数组，编写一个函数来计算它们的交集。
+ * 
+ * 示例 1:
+ * 
+ * 输入: nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出: [2]
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出: [9,4]
+ * 
+ * 说明:
+ * 
+ * 
+ * 输出结果中的每个元素一定是唯一的。
+ * 我们可以不考虑输出结果的顺序。
+ * 
+ * 
+ */
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    let res = []
+    nums1 = Array.from(new Set(nums1))
+    nums2 = Array.from(new Set(nums2))
+    if(nums1.length < nums2.length) {
+        for(let i of nums1) {
+            if(nums2.includes(i)) {
+                res.push(i)
+            }
+        }
+    } else {
+        for(let i of nums2) {
+            if(nums1.includes(i)) {
+                res.push(i)
+            }
+        }
+    }
+    return res
+};
+
