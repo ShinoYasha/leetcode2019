9445fd8ddaa7bddc09f38f1d5a9376faf67c07b7
shino <shinoyasha@163.com>
2019-04-14 16:48:36 +0800

    leetcode 12 350

---
+++ javascript/350.两个数组的交集-ii.js
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode.cn id=350 lang=javascript
+ *
+ * [350] 两个数组的交集 II
+ *
+ * https://leetcode-cn.com/problems/intersection-of-two-arrays-ii/description/
+ *
+ * algorithms
+ * Easy (40.43%)
+ * Total Accepted:    29.8K
+ * Total Submissions: 73.7K
+ * Testcase Example:  '[1,2,2,1]\n[2,2]'
+ *
+ * 给定两个数组，编写一个函数来计算它们的交集。
+ * 
+ * 示例 1:
+ * 
+ * 输入: nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出: [2,2]
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出: [4,9]
+ * 
+ * 说明：
+ * 
+ * 
+ * 输出结果中每个元素出现的次数，应与元素在两个数组中出现的次数一致。
+ * 我们可以不考虑输出结果的顺序。
+ * 
+ * 
+ * 进阶:
+ * 
+ * 
+ * 如果给定的数组已经排好序呢？你将如何优化你的算法？
+ * 如果 nums1 的大小比 nums2 小很多，哪种方法更优？
+ * 如果 nums2 的元素存储在磁盘上，磁盘内存是有限的，并且你不能一次加载所有的元素到内存中，你该怎么办？
+ * 
+ * 
+ */
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+    nums1.sort(function(a,b) {return a - b})
+    nums2.sort(function(a,b) {return a - b})
+    let len1 = nums1.length
+    let len2 = nums2.length
+    let i = 0, j = 0
+    let res =[]
+    while(i < len1 && j < len2) {
+        if(nums1[i] === nums2[j]) {
+            res.push(nums1[i])
+            i ++
+            j ++
+        } else if(nums1[i] > nums2[j]) {
+            j ++
+        } else {
+            i ++
+        }
+    }
+    return res
+};
+
