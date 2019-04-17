5226c2daf8b2c9ef075cef27df26195319af8dc8
shino <shinoyasha@163.com>
2019-04-17 22:39:49 +0800

    leetcode61

---
+++ javascript/61.旋转链表.js
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode.cn id=61 lang=javascript
+ *
+ * [61] 旋转链表
+ *
+ * https://leetcode-cn.com/problems/rotate-list/description/
+ *
+ * algorithms
+ * Medium (37.52%)
+ * Total Accepted:    12.9K
+ * Total Submissions: 34.2K
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * 给定一个链表，旋转链表，将链表每个节点向右移动 k 个位置，其中 k 是非负数。
+ * 
+ * 示例 1:
+ * 
+ * 输入: 1->2->3->4->5->NULL, k = 2
+ * 输出: 4->5->1->2->3->NULL
+ * 解释:
+ * 向右旋转 1 步: 5->1->2->3->4->NULL
+ * 向右旋转 2 步: 4->5->1->2->3->NULL
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: 0->1->2->NULL, k = 4
+ * 输出: 2->0->1->NULL
+ * 解释:
+ * 向右旋转 1 步: 2->0->1->NULL
+ * 向右旋转 2 步: 1->2->0->NULL
+ * 向右旋转 3 步: 0->1->2->NULL
+ * 向右旋转 4 步: 2->0->1->NULL
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var rotateRight = function(head, k) {
+    // head为null 直接返回
+    if(head == null) {
+        return null
+    }
+    // 计算链表长度，对k取模
+    let n = head
+    let len = 0
+    while(n != null) {
+        n = n.next
+        len ++
+    }
+    k = k % len
+    // 快慢节点 找到需要断开的位置
+    let fast = head
+    let slow = head
+    for(let i = 0; i < k; i++) {
+        fast = fast.next
+    }
+    while(fast.next) {
+        fast = fast.next
+        slow = slow.next
+    }
+    fast.next = head
+    fast = slow.next
+    slow.next = null
+    return fast
+};
+
